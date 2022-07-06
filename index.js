4ed6bf9c5f9b0ece309205a26e96ed40981f5e85
Graham <grh.mrg@gmail.com>
2022-07-06 22:12:32 +0200

    first

---
+++ index.js
@@ -0,0 +1,40 @@
+let resultado = document.getElementById('resultado');
+let button = document.getElementById('btn');
+button.addEventListener('click', main);
+
+
+function main() {
+    let km = parseInt(document.getElementById('km').value);
+    let dias = parseInt(document.getElementById('dias').value);
+    let personas = parseInt(document.getElementById('personas').value)
+    calculadora(km,dias,personas);
+}
+
+function calculadora(km,d,pers = 1) {
+
+    console.log(typeof km, km);
+    console.log(typeof d, d);
+    console.log(typeof pers, pers)
+    
+    let dias = d * 30;
+    let tramo1 = 0;
+    let tramo2 = 0;
+    let suma = 0;
+    
+    while(km > 100) {
+        tramo1++;
+        km--;
+    }
+    tramo1 = tramo1 * 0.18;
+    
+    while (km <= 100 && km > 30 ) {
+        tramo2++;
+        km--;
+    }
+    tramo2 = tramo2 * 0.3;
+    
+    suma = (tramo1 + tramo2 + dias) / pers;
+
+    resultado.innerText = suma + ' euros';
+
+}
