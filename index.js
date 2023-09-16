fa218386c0f3a2501151507f8c3b29c57fde64ec
Grehhh <32269724+Grehhh@users.noreply.github.com>
2023-09-16 20:47:52 +0200

    Update tramos

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function calculadora(km,d,pers = 1) {
     console.log(typeof d, d);
     console.log(typeof pers, pers)
     
-    let dias = d * 30;
+    let dias = d * 24;
     let tramo1 = 0;
     let tramo2 = 0;
     let suma = 0;
@@ -25,13 +25,13 @@ function calculadora(km,d,pers = 1) {
         tramo1++;
         km--;
     }
-    tramo1 = tramo1 * 0.18;
+    tramo1 = tramo1 * 0.3;
     
     while (km <= 100 && km > 30 ) {
         tramo2++;
         km--;
     }
-    tramo2 = tramo2 * 0.3;
+    tramo2 = tramo2 * 0.2;
     
     suma = (tramo1 + tramo2 + dias) / pers;
 
